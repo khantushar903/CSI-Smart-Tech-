4867bbac37783d7ad1fb81c778b61b0bf3d7112a
khantushar903 <khantushar903@gmail.com>
2026-04-11 13:34:37 +0600

    fix issues

--- components/csi/newsletter-form.tsx
+++ components/csi/newsletter-form.tsx
@@ -32,7 +32,7 @@ export default function NewsletterForm() {
     try {
       await emailjs.send(
         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
-        process.env.NEXT_PUBLIC_EMAILJS_NEWSLETTER_TEMPLATE_ID || "",
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
         {
           from_name: data.name,
           email: data.email,
